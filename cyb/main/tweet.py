df20edc80441852fdf709cecc06445def5bd27a1
Luc <49071530+thankYurLucK@users.noreply.github.com>
2020-02-24 13:17:31 +0100

    Update tweet.py
    fix

--- cyb/main/tweet.py
+++ cyb/main/tweet.py
@@ -4,7 +4,7 @@ from sentence.quote import build_Quote_Tweet_Body,searchTweets_For_Quote,getQuot
 
 def commentWithQutoes(api:tweepy.api):
     quote=getQuote()
-    tweet=searchTweets_For_Quote(api,quote)
+    tweet=searchTweets_For_Quote(quote,api)
     message=build_Quote_Tweet_Body(quote)
 
     api.update_status(message,in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True)
